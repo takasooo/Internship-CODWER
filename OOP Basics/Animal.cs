6a1fcbb8a837e29ce85e74243de2419d535eb0f1
takasooo <108112631+takasooo@users.noreply.github.com>
2023-09-16 20:31:48 +0300

    Added Project OOP Basics

---
+++ OOP Basics/Animal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basics
+{
+    public abstract class Animal
+    {
+        public string name;
+        public readonly decimal weight;
+        private readonly Dimensions dimension;
+        protected List<Food> stomach = new List<Food>();
+        public readonly decimal speed;
+
+
+
+        public struct Dimensions
+        {
+            public readonly decimal length, width, height;
+
+            public Dimensions(decimal length, decimal width, decimal height)
+            {
+                this.length = length;
+                this.width = width;
+                this.height = height;
+            }
+        }
+
+        public Animal(string name, decimal weight, Dimensions dimensions, decimal speed)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.dimension = dimensions;
+            this.speed = speed;
+        }
+
+        public void IsEating(Food food)
+        {
+            decimal foodMaxWeight = weight / 8;
+
+            if (food.weight <= foodMaxWeight)
+
+                stomach.Add(food);
+            Console.WriteLine("Is eating.");
+
+        }
+
+        public abstract double Energy();
+
+        public void IsRunning(Food food, decimal distance)
+        {
+            decimal time;
+
+
+            time = (distance / (speed / food.energy));
+            Console.WriteLine("The distance will be traveled in " + time + "seconds");
+        }
+    }
+
+    public class Herbivore : Animal
+    {
+        public Herbivore(string name,  decimal weight, Dimensions dimensions, decimal speed) : base (name, weight, dimensions, speed)
+        {
+        }
+        
+        public override double Energy()
+            f
+        {
+            return 0.5 + (1 / 3) * ((decimal)((float)stomach.Average(food => double)).weight)
+        }
+    }
+
+    public class Omnivorous : Animal
+    {
+        public Herbivore(string name, decimal weight, Dimensions dimensions, decimal speed) : base(name, weight, dimensions, speed)
+        {
+        }
+    }
+
+    public class Carnivore : Animal
+    {
+        public Herbivore(string name, decimal weight, Dimensions dimensions, decimal speed) : base(name, weight, dimensions, speed)
+        {
+        }
+        public override double Energy()
+        {
+
+        }
+    }
+}
+
+
+
+
---
+++ OOP Basics/Food.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basics
+{
+    public abstract class Food
+    {
+        public decimal weight;
+        public decimal energy;
+        public decimal maxEnergy = 0.05m;
+
+        public Food(decimal weight, decimal energy)
+        {
+            this.weight = weight;
+            if (energy >= 0 && energy <= 0.05m)
+            {
+                this.energy = energy;
+            } else
+            {
+                throw new ArgumentException("Cantitatea energiei poate fi intre 0 si 0.05");
+            }
+        }
+
+    }
+
+    public class Meat : Food
+    {
+        public Meat (decimal weight, decimal energy) : base (weight, energy)
+        {
+
+        }
+    }
+
+    public class Plants : Food
+    {
+        public Plants(decimal weight, decimal energy) : base(weight, energy)
+        {
+
+        }
+    }
+}
